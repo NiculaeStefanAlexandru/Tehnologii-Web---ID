083364136d667ead4913f5b06c03a99a9a7b5a55
Niculae Stefan <niculaestefan18@stud.ase.ro>
2024-10-12 11:10:30 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+function test() {
+    if(true) {
+        var var1 = 1;
+        let let1 = 2;
+        const const1 = 3;
+    }
+
+    console.log(var1);
+    console.log(let1);
+    console.log(const1);
+}
+
+test();
